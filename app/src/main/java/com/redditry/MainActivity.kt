52fafa87c986039802f401a4b7d4969b32faf47f
clement <clement.chaban@screeb.app>
2023-02-20 11:52:46 +0100

    Fix add intent filter

--- app/src/main/java/com/redditry/MainActivity.kt
+++ app/src/main/java/com/redditry/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        setContentView(R.layout.activity_main)
         LoginActivity.start(this,stopAutoLogin = true)
     }
 }
