06197967f3d4cedbae2c5709c5990955b3b35dab
Clément <80167759+cley44@users.noreply.github.com>
2023-02-21 15:00:06 +0100

    Feat 2 login view (#16)
    * Feat login view
    
    * Feat Oauth reddit
    
    * Fix add intent filter
    
    * Feat working login with redirect
    
    * Fix make url template
    
    * Introduce Post component layout (#15)
    
    Support multiple attributes but doesn't handle the layout required for the profile view
    
    Close #14
    
    * Feat login view
    
    * Fix rebase
    
    * Lint
    
    * Fix version
    
    ---------
    
    Co-authored-by: clement <clement.chaban@screeb.app>
    Co-authored-by: AndreasLrx <72009553+AndreasLrx@users.noreply.github.com>

---
+++ app/src/main/java/com/redditry/LoginActivity.kt
@@ -0,0 +1,47 @@
+package com.redditry
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.redditry.databinding.LoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: LoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginRegister.setOnClickListener {
+            val urlString = "https://www.reddit.com/register/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+            startActivity(intent)
+        }
+
+        val sharedPreferences = getSharedPreferences("redditry", MODE_PRIVATE)
+        var token = sharedPreferences.getString("redditToken", "")
+        val uri = intent.data
+        if (uri != null) {
+            val params = uri.encodedQuery
+            token = params?.split("=")?.get(2)
+            val myEdit = sharedPreferences.edit()
+            myEdit.putString("redditToken", token.toString())
+            myEdit.apply()
+            binding.loginRegister.text = token
+        }
+        if (token != "" && token != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.loginButton.setOnClickListener {
+            val redirectUri = "http://localhost:3000/api/login"
+            val clientId = "s-w2UUr5ZeLMbnI7lOMmwg"
+            val scope = "identity"
+            val urlString = "https://www.reddit.com/api/v1/authorize.compact?client_id=$clientId&response_type=code&state=%22random%22&redirect_uri=$redirectUri&duration=permanent&scope=$scope"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+            startActivity(intent)
+        }
+    }
+}
