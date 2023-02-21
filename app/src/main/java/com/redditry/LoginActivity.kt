ca8e508e1447dd0d2c48635bbe5129184e422734
clement <clement.chaban@screeb.app>
2023-02-21 14:43:24 +0100

    Lint

--- app/src/main/java/com/redditry/LoginActivity.kt
+++ app/src/main/java/com/redditry/LoginActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.redditry.databinding.LoginBinding
 
-
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: LoginBinding
