482fa145d3d76ceedca253a31660195d877a3b80
clement <clement.chaban@screeb.app>
2023-02-25 13:43:46 +0100

    Fix authentifcation get token instead of code

--- app/src/main/java/com/redditry/LoginActivity.kt
+++ app/src/main/java/com/redditry/LoginActivity.kt
@@ -4,11 +4,27 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.redditry.databinding.LoginBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Credentials
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: LoginBinding
+    private var client = OkHttpClient()
+    private var gson = Gson()
+    private val redirectUri = "http://localhost:3000/api/login"
+    private val clientId = "s-w2UUr5ZeLMbnI7lOMmwg"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,23 +37,57 @@ class LoginActivity : AppCompatActivity() {
         }
 
         val sharedPreferences = getSharedPreferences("redditry", MODE_PRIVATE)
-        var token = sharedPreferences.getString("redditToken", "")
+        val token = sharedPreferences.getString("redditToken", "")
         val uri = intent.data
-        if (uri != null) {
+        if (uri != null && token == "") {
             val params = uri.encodedQuery
-            token = params?.split("=")?.get(2)
-            val myEdit = sharedPreferences.edit()
-            myEdit.putString("redditToken", token.toString())
-            myEdit.apply()
-            binding.loginRegister.text = token
+            val code = params?.split("=")?.get(2)
+
+            val formBody: RequestBody = FormBody.Builder()
+                    .add("grant_type","authorization_code")
+                    .add("code", code!!)
+                    .add("redirect_uri", redirectUri)
+                    .build()
+            val credential: String = Credentials.basic(clientId, "")
+
+            val request = Request.Builder()
+                    .url("https://www.reddit.com/api/v1/access_token")
+                    .addHeader("Authorization",credential)
+                    .post(formBody)
+                    .build()
+
+            try {
+
+                val response = client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                            val redditTokenResponse = gson.fromJson(response.body?.string(), RedditTokenResponse::class.java)
+                            val myEdit = sharedPreferences.edit()
+                            myEdit.putString("redditToken", redditTokenResponse.access_token)
+                            myEdit.apply()
+
+                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                })
+
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
         if (token != "" && token != null) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
         binding.loginButton.setOnClickListener {
-            val redirectUri = "http://localhost:3000/api/login"
-            val clientId = "s-w2UUr5ZeLMbnI7lOMmwg"
             val scope = "identity"
             val urlString = "https://www.reddit.com/api/v1/authorize.compact?client_id=$clientId&response_type=code&state=%22random%22&redirect_uri=$redirectUri&duration=permanent&scope=$scope"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
@@ -45,3 +95,11 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
+
+class RedditTokenResponse (
+    val access_token:String,
+    val token_type:String,
+    val expires_in:String,
+    val scope:String,
+    val refresh_token:String
+)
