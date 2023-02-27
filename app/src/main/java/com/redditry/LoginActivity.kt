bbc6787a4c8292f505fcfae5233805f1e4e0c06c
clement <clement.chaban@screeb.app>
2023-02-27 15:59:43 +0100

    Feat login with moshi and retrofit

--- app/src/main/java/com/redditry/LoginActivity.kt
+++ app/src/main/java/com/redditry/LoginActivity.kt
@@ -3,28 +3,34 @@ package com.redditry
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
 import com.redditry.databinding.LoginBinding
-import okhttp3.Call
-import okhttp3.Callback
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import okhttp3.Credentials
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.Response
-import java.io.IOException
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: LoginBinding
-    private var client = OkHttpClient()
-    private var gson = Gson()
-    private val redirectUri = "http://localhost:3000/api/login"
-    private val clientId = "s-w2UUr5ZeLMbnI7lOMmwg"
-
+    private val redirectUri = BuildConfig.REDDIT_REDIRECT_URI
+    private val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.reddit.com/api/v1/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,66 +46,74 @@ class LoginActivity : AppCompatActivity() {
         val token = sharedPreferences.getString("redditToken", "")
         val uri = intent.data
         if (uri != null && token == "") {
+            startAnimation()
             val params = uri.encodedQuery
             val code = params?.split("=")?.get(2)
 
-            val formBody: RequestBody = FormBody.Builder()
-                    .add("grant_type","authorization_code")
-                    .add("code", code!!)
-                    .add("redirect_uri", redirectUri)
-                    .build()
-            val credential: String = Credentials.basic(clientId, "")
 
-            val request = Request.Builder()
-                    .url("https://www.reddit.com/api/v1/access_token")
-                    .addHeader("Authorization",credential)
-                    .post(formBody)
-                    .build()
+            val api = retrofit.create(ApiReddit::class.java)
 
-            try {
+            GlobalScope.launch {
+                val resp = api.login(
+                    Credentials.basic(BuildConfig.REDDIT_CLIENT_ID, ""),
+                    code = code!!,
+                    redirect_uri = redirectUri
+                )
+                        .execute()
+                if (resp.isSuccessful) {
+                    val myEdit = sharedPreferences.edit()
+                    myEdit.putString("redditToken", resp.body()?.access_token)
+                    myEdit.apply()
 
-                val response = client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                            val redditTokenResponse = gson.fromJson(response.body?.string(), RedditTokenResponse::class.java)
-                            val myEdit = sharedPreferences.edit()
-                            myEdit.putString("redditToken", redditTokenResponse.access_token)
-                            myEdit.apply()
-
-                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                            startActivity(intent)
-                        }
-                    }
-                })
-
-
-            } catch (e: IOException) {
-                e.printStackTrace()
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                stopAnimation()
             }
+
         }
         if (token != "" && token != null) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
         binding.loginButton.setOnClickListener {
-            val scope = "identity"
-            val urlString = "https://www.reddit.com/api/v1/authorize.compact?client_id=$clientId&response_type=code&state=%22random%22&redirect_uri=$redirectUri&duration=permanent&scope=$scope"
+            val scope = BuildConfig.REDDIT_SCOPE
+            val urlString = "https://www.reddit.com/api/v1/authorize.compact?client_id=" + BuildConfig.REDDIT_CLIENT_ID + "&response_type=code&state=%22random%22&redirect_uri=$redirectUri&duration=permanent&scope=$scope"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
             startActivity(intent)
         }
     }
-}
 
-class RedditTokenResponse (
-    val access_token:String,
-    val token_type:String,
-    val expires_in:String,
-    val scope:String,
-    val refresh_token:String
-)
+    private fun startAnimation() {
+        binding.loginLogo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate))
+    }
+
+    private fun stopAnimation() {
+        binding.loginLogo.clearAnimation()
+    }
+
+    interface ApiReddit {
+        @POST("access_token")
+        @FormUrlEncoded
+        fun login(
+            @Header("Authorization") authorization: String,
+            @Field("grant_type") grant_type: String = "authorization_code",
+            @Field("code") code: String,
+            @Field("redirect_uri") redirect_uri: String
+        ): Call<RedditTokenResponse>
+    }
+
+    @JsonClass(generateAdapter = true)
+    data class RedditTokenResponse(
+        @Json(name = "access_token")
+        val access_token: String?,
+        @Json(name = "token_type")
+        val token_type: String?,
+        @Json(name = "expires_in")
+        val expires_in: String?,
+        @Json(name = "scope")
+        val scope: String?,
+        @Json(name = "refresh_token")
+        val refresh_token: String?
+    )
+}
