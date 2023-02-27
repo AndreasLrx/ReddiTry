acb7ba155a7ab31e8feae8beb8f427d15c00ee45
Lisa Bourdon <lisa.bourdon@epitech.eu>
2023-02-27 14:26:33 +0100

    fix: hardcoded string + id

---
+++ app/src/main/java/com/redditry/SubredditActivity.kt
@@ -0,0 +1,11 @@
+package com.redditry
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubredditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subreddit)
+    }
+}
