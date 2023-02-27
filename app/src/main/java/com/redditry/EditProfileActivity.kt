79cbb0172c58a2cad362ddacf198bba10ea99ad7
Lisa Bourdon <lisa.bourdon@epitech.eu>
2023-02-27 17:29:26 +0100

    fix: change id

---
+++ app/src/main/java/com/redditry/EditProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.redditry
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+
+}
