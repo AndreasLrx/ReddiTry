105f511da9dc6f31a826fd97d306bf2fbe4fc507
Andreas Leroux <andreas.leroux@epitech.eu>
2023-02-28 16:33:25 +0100

    Refactored layouts id and introduce binding variable in all components
    Linked: #19

--- app/src/main/java/com/redditry/SubredditActivity.kt
+++ app/src/main/java/com/redditry/SubredditActivity.kt
@@ -2,10 +2,13 @@ package com.redditry
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.redditry.databinding.ActivitySubredditBinding
 
 class SubredditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubredditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_subreddit)
+        binding = ActivitySubredditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
