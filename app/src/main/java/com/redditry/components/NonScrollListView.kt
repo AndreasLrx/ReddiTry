aa3552ed5b811364215966ec424b7925702194ac
Andreas Leroux <andreas.leroux@epitech.eu>
2023-03-06 20:18:07 +0100

    Implemented PostList in profile/subreddit views
    Linked #9

---
+++ app/src/main/java/com/redditry/components/NonScrollListView.kt
@@ -0,0 +1,27 @@
+package com.redditry.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.ListView
+
+
+class NonScrollListView constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : ListView(context, attrs) {
+    var scrollable: Boolean = true
+
+    init {}
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val maxHeight = if (!scrollable) MeasureSpec.makeMeasureSpec(
+            Int.MAX_VALUE shr 2, MeasureSpec.AT_MOST
+        ) else heightMeasureSpec
+        super.onMeasure(widthMeasureSpec, maxHeight)
+        if (!scrollable) {
+            val params: ViewGroup.LayoutParams = layoutParams
+            params.height = measuredHeight
+        }
+    }
+}
