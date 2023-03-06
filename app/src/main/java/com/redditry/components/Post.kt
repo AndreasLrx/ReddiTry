db592737bb4af472a39331ee6adde6e4e386a97e
Andreas Leroux <andreas.leroux@epitech.eu>
2023-03-06 10:59:49 +0100

    Add post setData function
    Close #37

--- app/src/main/java/com/redditry/components/Post.kt
+++ app/src/main/java/com/redditry/components/Post.kt
@@ -1,6 +1,7 @@
 package com.redditry.components
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import android.widget.RelativeLayout
 import androidx.core.content.ContextCompat
 import com.redditry.R
 import com.redditry.databinding.ComponentPostBinding
+import com.redditry.redditAPI.PostData
 
 class Post @JvmOverloads constructor(
     context: Context,
@@ -18,7 +20,129 @@ class Post @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
 
+    enum class Format {
+        Expanded, Small
+    }
+
+    enum class Color {
+        Purple, Orange, HalfTransparent
+    }
+
+
     private var binding: ComponentPostBinding
+    private var _format: Format = Format.Expanded
+    private var _backgroundColor: Color = Color.Purple
+    private var _icon: Drawable? = null
+    private var _subredditName: String? = null
+    private var _userName: String? = null
+    private var _title: String? = null
+    private var _image: Drawable? = null
+    private var _content: String? = null
+    private var _votes: Int = 0
+    private var _comments: Int = 0
+
+
+    var format: Format
+        get() = _format
+        set(value) {
+            _format = value
+            if (_format == Format.Expanded) {
+                if (image != null)
+                    binding.content.maxLines = 3
+                else
+                    binding.content.maxLines = 8
+            }
+        }
+    var backgroundColor: Color
+        get() = _backgroundColor
+        set(value) {
+            _backgroundColor = value
+            when (_backgroundColor) {
+                Color.Purple -> setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.purple_light
+                    )
+                )
+                Color.Orange -> setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.beige_light
+                    )
+                )
+                Color.HalfTransparent -> setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.semi_transparent
+                    )
+                )
+            }
+        }
+    var icon: Drawable?
+        get() = _icon
+        set(value) {
+            _icon = value
+            binding.subredditIcon.setImageDrawable(_icon)
+        }
+    var subredditName: String?
+        get() = _subredditName
+        set(value) {
+            _subredditName = if (value == "")
+                null
+            else
+                value
+            binding.subredditName.text = _subredditName
+        }
+    var userName: String?
+        get() = _userName
+        set(value) {
+            _userName = value
+            binding.userName.text = _userName
+        }
+    var title: String?
+        get() = _title
+        set(value) {
+            _title = value
+            binding.title.text = _title
+        }
+    var image: Drawable?
+        get() = _image
+        set(value) {
+            _image = value
+            // update format bc it depends on image
+            format = format
+            if (image != null) {
+                binding.image.visibility = View.VISIBLE
+                binding.image.setImageDrawable(image)
+            } else
+                binding.image.visibility = View.GONE
+        }
+    var content: String?
+        get() = _content
+        set(value) {
+            _content = if (value == "")
+                null
+            else
+                value
+            if (content != null) {
+                binding.content.visibility = View.VISIBLE
+                binding.content.text = content
+            } else
+                binding.content.visibility = View.GONE
+        }
+    var votes: Int
+        get() = _votes
+        set(value) {
+            _votes = value
+            binding.votes.text = votes.toString()
+        }
+    var comments: Int
+        get() = _comments
+        set(value) {
+            _comments = value
+            binding.comments.text = comments.toString()
+        }
+
 
     init {
         LayoutInflater.from(context).inflate(R.layout.component_post, this, true)
@@ -30,84 +154,66 @@ class Post @JvmOverloads constructor(
                 context.obtainStyledAttributes(it, R.styleable.Post, 0, 0)
 
             // Post format
-            val postFormat = styledAttributes.getInt(R.styleable.Post_post_format, 0)
-            // Small
-            if (postFormat == 1) {
-                if (styledAttributes.hasValue(R.styleable.Post_image))
-                    binding.content.maxLines = 3
-                else
-                    binding.content.maxLines = 8
-            }
-
+            format = Format.values()[styledAttributes.getInt(R.styleable.Post_post_format, 0)]
             // Background Color
-            val backgroundColorId = styledAttributes.getInt(R.styleable.Post_post_color, 0)
-            when (backgroundColorId) {
-                0 -> setBackgroundColor(ContextCompat.getColor(context, R.color.purple_light))
-                1 -> setBackgroundColor(ContextCompat.getColor(context, R.color.beige_light))
-                2 -> setBackgroundColor(ContextCompat.getColor(context, R.color.semi_transparent))
-            }
-
+            backgroundColor =
+                Color.values()[styledAttributes.getInt(R.styleable.Post_post_color, 0)]
             // Subreddit icon
-            val icon = styledAttributes.getDrawable(R.styleable.Post_subreddit_icon)
-            if (icon != null)
-                binding.subredditIcon.setImageDrawable(icon)
+            icon = styledAttributes.getDrawable(R.styleable.Post_subreddit_icon)
             // Subreddit name
-            binding.subredditName.text =
-                styledAttributes.getString(R.styleable.Post_subreddit)
+            subredditName = styledAttributes.getString(R.styleable.Post_subreddit)
             // Sender name
-            binding.userName.text =
-                styledAttributes.getString(R.styleable.Post_sender)
+            userName = styledAttributes.getString(R.styleable.Post_sender)
             // Post title
-            binding.title.text =
-                styledAttributes.getString(R.styleable.Post_post_title)
-
+            title = styledAttributes.getString(R.styleable.Post_post_title)
             // Post image (optional)
-            val image = styledAttributes.getDrawable(R.styleable.Post_image)
-            if (image != null) {
-                binding.image.visibility = View.VISIBLE
-                binding.image.setImageDrawable(image)
-            } else
-                binding.image.visibility = View.GONE
-
+            image = styledAttributes.getDrawable(R.styleable.Post_image)
             // Post content (optional)
-            val content = styledAttributes.getString(R.styleable.Post_post_content)
-            if (content != null) {
-                binding.content.visibility = View.VISIBLE
-                binding.content.text = content
-            } else
-                binding.content.visibility = View.GONE
-
+            content = styledAttributes.getString(R.styleable.Post_post_content)
             // Post votes count
-            binding.votes.text =
-                styledAttributes.getInt(R.styleable.Post_votes_count, 0).toString()
+            votes = styledAttributes.getInt(R.styleable.Post_votes_count, 0)
             // Post comment count
-            binding.comments.text =
-                styledAttributes.getInt(R.styleable.Post_comments_count, 0).toString()
-
-            binding.upvoteButton.setOnClickListener(
-                createButtonClickListener {
-                    println(
-                        "Up Vote clicked"
-                    )
-                }
-            )
-            binding.downVoteButton.setOnClickListener(
-                createButtonClickListener {
-                    println(
-                        "Down Vote clicked"
-                    )
-                }
-            )
-            binding.commentButton.setOnClickListener(
-                createButtonClickListener {
-                    println(
-                        "Comment clicked"
-                    )
-                }
-            )
-
+            comments = styledAttributes.getInt(R.styleable.Post_comments_count, 0)
             styledAttributes.recycle()
         }
+
+        binding.upvoteButton.setOnClickListener(
+            createButtonClickListener {
+                println(
+                    "Up Vote clicked"
+                )
+            }
+        )
+        binding.downVoteButton.setOnClickListener(
+            createButtonClickListener {
+                println(
+                    "Down Vote clicked"
+                )
+            }
+        )
+        binding.commentButton.setOnClickListener(
+            createButtonClickListener {
+                println(
+                    "Comment clicked"
+                )
+            }
+        )
+    }
+
+    fun setData(data: PostData) {
+        subredditName = data.subreddit_name_prefixed
+        title = data.title
+        content = data.content
+        votes = data.voteUp /* - data.voteDown */
+        comments = data.numComments
+        userName = "u/" + data.author
+
+        /*
+        format
+        backgroundColor
+        image
+        icon
+         */
     }
 
     private fun createButtonClickListener(onEndCallback: () -> Unit): View.OnClickListener {
