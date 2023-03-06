71662813eaf4ca47b711e844790131f16dcd86f0
Andreas Leroux <andreas.leroux@epitech.eu>
2023-03-06 16:50:37 +0100

    Introduce post list component
    Linked: #9

---
+++ app/src/main/java/com/redditry/LazyLoader.kt
@@ -0,0 +1,41 @@
+package com.redditry
+
+import android.widget.AbsListView
+
+abstract class LazyLoader constructor() : AbsListView.OnScrollListener {
+    private var loading = true
+    private var previousTotal = 0
+    private var threshold = 0
+
+    override fun onScrollStateChanged(view: AbsListView, scrollState: Int) {}
+    override fun onScroll(
+        view: AbsListView, firstVisibleItem: Int,
+        visibleItemCount: Int, totalItemCount: Int
+    ) {
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                // the loading has finished
+                loading = false
+                previousTotal = totalItemCount
+            }
+        }
+
+        // check if the List needs more data
+        if (!loading && (firstVisibleItem + visibleItemCount) >= (totalItemCount - threshold)) {
+            loading = true
+
+            // List needs more data. Go fetch !!
+            loadMore(
+                view, firstVisibleItem,
+                visibleItemCount, totalItemCount
+            )
+        }
+    }
+
+    // Called when the user is nearing the end of the ListView
+    // and the ListView is ready to add more items.
+    abstract fun loadMore(
+        view: AbsListView?, firstVisibleItem: Int,
+        visibleItemCount: Int, totalItemCount: Int
+    )
+}
