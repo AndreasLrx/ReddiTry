1bd3271b05ca7db86c7cedfc822e19507f323626
Lisa Bourdon <lisa.bourdon@epitech.eu>
2023-03-11 20:44:40 +0100

    fix

--- app/src/main/java/com/redditry/components/AdapterPostList.kt
+++ app/src/main/java/com/redditry/components/AdapterPostList.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 
 class AdapterPostList(private val context: Context, posts: ArrayList<com.redditry.redditAPI.Post>) :
-        BaseAdapter() {
+    BaseAdapter() {
     enum class Color {
         Alternate,
         HalfTransparent
@@ -50,13 +50,13 @@ class AdapterPostList(private val context: Context, posts: ArrayList<com.redditr
     // set the post background color
     fun loadPosts(start: Int = 0) {
         if (postViews.size < start)
-                throw java.lang.IndexOutOfBoundsException(
-                        "Try to load posts from " +
-                                start.toString() +
-                                " where list have only " +
-                                postViews.size +
-                                " elements."
-                )
+            throw java.lang.IndexOutOfBoundsException(
+                "Try to load posts from " +
+                    start.toString() +
+                    " where list have only " +
+                    postViews.size +
+                    " elements."
+            )
 
         postViews.ensureCapacity(postData.size)
         for (i in start until postData.size) {
